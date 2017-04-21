fb90ad00e46ab1255f9bde4271b29e1b8ed0fa3a
Sizhuo Zhang <szzhang@mit.edu>
2017-04-21 10:01:38 -0400

    add mit license

--- xilinx/reset_regs/reset_guard.v
+++ xilinx/reset_regs/reset_guard.v
@@ -1,7 +1,29 @@
 
+// Copyright (c) 2017 Massachusetts Institute of Technology
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // This module outputs a 1-bit signal
 // The signal is initially 0 after programming the FPGA
-// XXX: hopefully everything is inited to 0 after programming
+// XXX: everything should be inited to 0 after programming
 
 // When a reset arrives, the module starts counting
 // In N cycles after the reset, the signal becomes 1 
